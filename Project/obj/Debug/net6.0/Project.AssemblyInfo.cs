683cb22dd78a9f81da3950cea3c9f8cde63baf70
Dipto216 <diptosaha216@gmail.com>
2024-04-28 19:01:40 +0600

    My Project

--- Project/obj/Debug/net6.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net6.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b4163016dd3c8e1e1baa94ea705c8ad6e110823")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
